aca20786a08d3df1855ea64bc4b61f3ba4a3a339
vishwasraiborde <vishwasraiborde@vishwass-mbp>
2022-04-14 17:21:50 +0100

    initial scaffold

---
+++ ecom-parent/src/main/java/com/ecom/parent/ParentApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaConsumer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKafkaConsumer1Application.class, args);
+	}
+
+}

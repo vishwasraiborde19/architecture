8e1a3716bf799c3f8d091fe6e636e2b8448f870b
vishwasraiborde <vishwasraiborde@vishwass-mbp>
2022-04-14 21:10:40 +0100

    test

--- ecom-product-service/src/main/java/com/ecom/product/EcomPaymentServiceApplication.java
+++ ecom-product-service/src/main/java/com/ecom/product/EcomPaymentServiceApplication.java
@@ -2,13 +2,19 @@ package com.ecom.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+//@EnableJpaRepositories("com.ecom.product.domain")
+//@ComponentScan(basePackages = { "com.ecom.product.domain" })
+//@EntityScan("com.ecom.product.domain")
 public class EcomPaymentServiceApplication {
 
 	public static void main(String[] args) {

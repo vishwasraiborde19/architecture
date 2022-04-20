fb6795b69b816ecb781d346cf3d7cd724304d533
vishwasraiborde <vishwasraiborde@vishwass-mbp>
2022-04-20 01:27:26 +0100

    more refractoring

--- ms-ecom-api-gateway-server/src/main/java/com/ecom/gateway/EcomApiGatewayyServerApplication.java
+++ ms-ecom-api-gateway-server/src/main/java/com/ecom/gateway/EcomApiGatewayyServerApplication.java
@@ -3,12 +3,9 @@ package com.ecom.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableHystrix
-@EnableHystrixDashboard
 public class EcomApiGatewayyServerApplication {
 
 	public static void main(String[] args) {

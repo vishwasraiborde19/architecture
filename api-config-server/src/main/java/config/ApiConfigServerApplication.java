ee5b5b220a74f06ed318df4b89561200bd4241bd
vishwasraiborde <vishwasraiborde@vishwass-mbp>
2022-05-13 12:07:30 +0100

    initial scaffold

---
+++ api-config-server/src/main/java/config/ApiConfigServerApplication.java
@@ -0,0 +1,13 @@
+package config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConfigServerApplication.class, args);
+	}
+
+}
